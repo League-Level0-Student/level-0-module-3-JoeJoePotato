9361d581a5f06de224b8f62beea0b247a3e2c382
JoeJoePotato <league@WTS10.local>
2018-05-30 17:22:25 -0700

    obedient robot final

--- src/extra/obedientRobot.java
+++ src/extra/obedientRobot.java
@@ -11,24 +11,24 @@ public class obedientRobot {
 
 		potato.penDown();
 		potato.miniaturize();
-String shape=JOptionPane.showInputDialog("What shape do you want the robot to draw?/n The options are square, circle and triangle.\n No caps please.");
-String color=JOptionPane.showInputDialog("What color do you want the robot to draw in?/n The options are red, green and blue.\n No caps please.");
+String shape=JOptionPane.showInputDialog("What shape do you want the robot to draw? The options are square, circle and triangle.\n No caps please.");
+String color=JOptionPane.showInputDialog("What color do you want the robot to draw in? The options are red, green and blue.\n No caps please.");
 
 if(color.equals("red")) {
 potato.setPenColor(255,0,0);
 }else if(color.equals("blue")) {
-potato.setPenColor(0,255,0);}
+potato.setPenColor(0,0,255);}
 else if(color.equals("green")) {
-potato.setPenColor(0,0,255);} else {
+potato.setPenColor(0,255,0);} else {
 potato.setRandomPenColor();}
 
-if(color.equals("square")) {
+if(shape.equals("square")) {
 drawSquare();
-}else if(color.equals("blue")) {
-potato.setPenColor(0,255,0);}
-else if(color.equals("green")) {
-potato.setPenColor(0,0,255);} else {
-potato.setRandomPenColor();}
+}else if(shape.equals("triangle")) {
+drawTriangle();}
+else if(shape.equals("circle")) {
+drawCircle();} else {
+JOptionPane.showMessageDialog(null, "Sorry, that shape is not compatible.");}
 	}
 
 	static void drawSquare() {
@@ -42,8 +42,8 @@ potato.setRandomPenColor();}
 	static void drawTriangle() {
 
 		for (int i = 0; i < 3; i++) {
-			potato.move(450);
-			potato.turn(60);
+			potato.move(250);
+			potato.turn(120);
 		}
 
 	}
